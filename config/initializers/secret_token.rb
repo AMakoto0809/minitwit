daf63db0c98bd2f6d895361b801f91ee4c0a2f67
makoto <makoto@makoto-no-MacBook-Pro.local>
2014-10-20 23:54:15 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Minitwit::Application.config.secret_token = 'd6f31413923968ca0d3c8c6eb636b1dac859fc91fe054200889e60cb9397504d2429e88e3fbfcd603a8c1436793a6922cd31b376682cec0e9a18cf19d2eb0276'
